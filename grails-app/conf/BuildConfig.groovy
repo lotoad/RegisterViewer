6fbb73c176e4aa6aed39295ac605116542b0f109
jtoddington <jtoddington@gmail.com>
2015-01-27 21:52:14 +0000

    Added tests and implementation.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -48,9 +48,15 @@ grails.project.dependency.resolution = {
 
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
-        // runtime 'mysql:mysql-connector-java:5.1.29'
+        runtime 'mysql:mysql-connector-java:5.1.29'
         // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
         test "org.grails:grails-datastore-test-support:1.0-grails-2.4"
+
+        //Elasticsearch Client deps.
+
+
+        //Mongo Client deps
+        //compile 'org.mongodb:mongo-java-driver:2.12.5'
     }
 
     plugins {
@@ -66,6 +72,10 @@ grails.project.dependency.resolution = {
         runtime ":hibernate4:4.3.5.5" // or ":hibernate:3.6.10.17"
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
+
+
+
+        compile ":mongodb:3.0.2"
 
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.9.0"
